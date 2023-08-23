a5ef3b5c35591dcf6c9d4f6b56fb758d46f6a137
Felicitas Pojtinger <felicitas@pojtinger.com>
2023-08-23 20:32:23 +0200

    feat: Add project list generated

---
+++ cmd/ps-gen-projects/main.go
@@ -0,0 +1,202 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"html"
+	"net/http"
+	"net/url"
+	"os"
+	"path"
+	"sort"
+	"strings"
+	"time"
+
+	"github.com/google/go-github/v42/github"
+	"golang.org/x/oauth2"
+	"gopkg.in/yaml.v2"
+)
+
+// Output
+type OutputProject struct {
+	URL         string   `yaml:"url"`
+	Title       string   `yaml:"title"`
+	Description string   `yaml:"description"`
+	Language    string   `yaml:"language"`
+	License     string   `yaml:"license"`
+	Date        string   `yaml:"date"`
+	Topics      []string `yaml:"topics"`
+	Stars       int      `yaml:"stars"`
+	Forks       int      `yaml:"forks"`
+	Issues      int      `yaml:"issues"`
+	Background  string   `yaml:"background"`
+	Icon        string   `yaml:"icon"`
+}
+
+type OutputCategory struct {
+	Title    string          `yaml:"title"`
+	Projects []OutputProject `yaml:"projects"`
+}
+
+// Input
+type InputCategory struct {
+	Title    string         `yaml:"title"`
+	Projects []InputProject `yaml:"projects"`
+}
+
+type InputProject struct {
+	Slug       string `yaml:"slug"`
+	Background string `yaml:"background"`
+	Icon       bool   `yaml:"icon"`
+}
+
+func main() {
+	src := flag.String("src", "projects.yaml", "Source YAML file")
+	api := flag.String("api", "https://api.github.com/", "GitHub/Gitea API endpoint to use (can also be set using the GITHUB_API env variable)")
+	cdn := flag.String("cdn", "https://raw.githubusercontent.com/", "GitHub/Gitea CDN endpoint to use (can also be set using the GITHUB_CDN env variable)")
+	token := flag.String("token", "", "GitHub/Gitea API access token (can also be set using the GITHUB_TOKEN env variable)")
+	user := flag.String("user", "pojntfx", "GitHub username (can also be set using the GITHUB_USER env variable)")
+
+	flag.Parse()
+
+	if *api == "" {
+		*api = os.Getenv("GITHUB_API")
+	}
+
+	if *cdn == "" {
+		*cdn = os.Getenv("GITHUB_CDN")
+	}
+
+	if *token == "" {
+		*token = os.Getenv("GITHUB_TOKEN")
+	}
+
+	if *user == "" {
+		*user = os.Getenv("GITHUB_USER")
+	}
+
+	input, err := os.ReadFile(*src)
+	if err != nil {
+		panic(err)
+	}
+
+	var parsedInput []InputCategory
+	if err := yaml.Unmarshal(input, &parsedInput); err != nil {
+		panic(err)
+	}
+
+	var httpClient *http.Client
+	if *token != "" {
+		httpClient = oauth2.NewClient(
+			context.Background(),
+			oauth2.StaticTokenSource(
+				&oauth2.Token{
+					AccessToken: *token,
+				},
+			),
+		)
+	}
+
+	client := github.NewClient(httpClient)
+	client.BaseURL, err = url.Parse(*api)
+	if err != nil {
+		panic(err)
+	}
+
+	parsedOutput := []OutputCategory{}
+	for _, inputCategory := range parsedInput {
+		outputCategory := OutputCategory{
+			Title:    inputCategory.Title,
+			Projects: []OutputProject{},
+		}
+
+		for _, inputProject := range inputCategory.Projects {
+			owner, repo := path.Split(inputProject.Slug)
+
+			project, _, err := client.Repositories.Get(context.Background(), strings.TrimSuffix(owner, "/"), repo)
+			if err != nil {
+				panic(err)
+			}
+
+			license := "UNLICENSED"
+			if l := project.GetLicense(); l != nil {
+				license = l.GetSPDXID()
+			}
+
+			commits, _, err := client.Repositories.ListCommits(context.Background(), strings.TrimSuffix(owner, "/"), repo, &github.CommitsListOptions{})
+			if err != nil {
+				panic(err)
+			}
+
+			latestCommitDate := project.GetPushedAt().Time
+			if len(commits) > 0 {
+				latestCommitDate = *commits[0].Commit.Author.Date
+			}
+
+			icon := ""
+			if inputProject.Icon {
+				icon = *cdn + owner + repo + "/" + project.GetDefaultBranch() + "/docs/icon-light.png"
+			}
+
+			outputCategory.Projects = append(outputCategory.Projects, OutputProject{
+				URL:         project.GetHTMLURL(),
+				Title:       project.GetFullName(),
+				Description: project.GetDescription(),
+				Language:    project.GetLanguage(),
+				License:     license,
+				Date:        latestCommitDate.Format(time.RFC3339),
+				Topics:      project.Topics,
+				Stars:       project.GetStargazersCount(),
+				Forks:       project.GetForksCount(),
+				Issues:      project.GetOpenIssuesCount(),
+				Background:  inputProject.Background,
+				Icon:        icon,
+			})
+		}
+
+		parsedOutput = append(parsedOutput, outputCategory)
+	}
+
+	markdownOutput := ""
+	for _, outputCategory := range parsedOutput {
+		sort.Slice(outputCategory.Projects, func(i, j int) bool {
+			return outputCategory.Projects[i].Stars > outputCategory.Projects[j].Stars
+		})
+
+		markdownOutput += "**" + html.EscapeString(outputCategory.Title) + ":**\n\n"
+
+		for _, project := range outputCategory.Projects {
+			parsedDate, err := time.Parse(time.RFC3339, project.Date)
+			if err != nil {
+				panic(err)
+			}
+			formattedDate := parsedDate.Format("2006")
+
+			iconMarkdown := ""
+			if project.Icon != "" {
+				iconMarkdown = fmt.Sprintf("<img alt=\"Icon\" src=\"%s\" height=\"30\" align=\"center\"> ", html.EscapeString(project.Icon))
+			}
+
+			displayedTitle := project.Title
+			if strings.Split(project.Title, "/")[0] == *user {
+				displayedTitle = strings.Split(project.Title, "/")[1] // Use just the repo name if owner matches the GitHub user
+			}
+
+			markdownLine := fmt.Sprintf("- <a display=\"inline\" target=\"_blank\" href=\"%s\"><b>%s%s</b></a> | ⭐ %d 🛠️ %s ⚖️ %s 📅 %s <br>%s\n",
+				html.EscapeString(project.URL),
+				iconMarkdown,
+				html.EscapeString(displayedTitle),
+				project.Stars,
+				html.EscapeString(project.Language),
+				html.EscapeString(project.License),
+				formattedDate,
+				html.EscapeString(project.Description))
+
+			markdownOutput += markdownLine
+		}
+		markdownOutput += "\n"
+	}
+
+	fmt.Print(markdownOutput)
+}
---
+++ go.mod
@@ -0,0 +1,18 @@
+module github.com/pojntfx/pojntfx
+
+go 1.20
+
+require (
+	github.com/google/go-github/v42 v42.0.0
+	golang.org/x/oauth2 v0.11.0
+	gopkg.in/yaml.v2 v2.4.0
+)
+
+require (
+	github.com/golang/protobuf v1.5.3 // indirect
+	github.com/google/go-querystring v1.1.0 // indirect
+	golang.org/x/crypto v0.12.0 // indirect
+	golang.org/x/net v0.14.0 // indirect
+	google.golang.org/appengine v1.6.7 // indirect
+	google.golang.org/protobuf v1.31.0 // indirect
+)
