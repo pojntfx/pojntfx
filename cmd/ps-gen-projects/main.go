8d74e3e2693fef86453afb63fa810190e85cbd3b
Felicitas Pojtinger <felicitas@pojtinger.com>
2025-12-29 10:58:36 -0800

    feat: Add basic inital support for Forgejo/Codeberg
    Signed-off-by: Felicitas Pojtinger <felicitas@pojtinger.com>

--- cmd/ps-gen-projects/main.go
+++ cmd/ps-gen-projects/main.go
@@ -32,6 +32,8 @@ type OutputProject struct {
 	Forks       int      `yaml:"forks"`
 	Issues      int      `yaml:"issues"`
 	Icon        string   `yaml:"icon"`
+	ForgeDomain string   `yaml:"forgeDomain"`
+	ForgeEmoji  string   `yaml:"forgeEmoji"`
 }
 
 type OutputCategory struct {
@@ -46,17 +48,30 @@ type InputCategory struct {
 }
 
 type InputProject struct {
-	Slug string `yaml:"slug"`
+	Repo string `yaml:"repo"`
 	Icon string `yaml:"icon"`
+}
+
+// Forges
+type ForgeConfig struct {
+	Domain string `yaml:"domain"`
+	Type   string `yaml:"type"` // github|forgejo
+	API    string `yaml:"api"`
+	CDN    string `yaml:"cdn"`
+	Emoji  string `yaml:"emoji"`
+}
+
+type ForgeSecret struct {
+	Domain string `yaml:"domain"`
+	Token  string `yaml:"token"`
 }
 
 func main() {
 	verbosity := flag.String("verbosity", "info", "Log level (debug, info, warn, error)")
-	src := flag.String("src", "projects.yaml", "Source YAML file")
-	api := flag.String("api", "https://api.github.com/", "GitHub/Forgejo API endpoint to use (can also be set using the FORGE_API env variable)")
-	cdn := flag.String("cdn", "https://raw.githubusercontent.com/", "GitHub/Forgejo CDN endpoint to use (can also be set using the FORGE_CDN env variable)")
-	token := flag.String("token", "", "GitHub/Forgejo API access token (can also be set using the FORGE_TOKEN env variable)")
-	user := flag.String("user", "pojntfx", "GitHub username (can also be set using the FORGE_USER env variable)")
+	projectsFile := flag.String("projects", "projects.yaml", "Projects configuration file")
+	forgesFile := flag.String("forges", "forges.yaml", "Forges configuration file")
+	secretsFile := flag.String("secrets", "secrets.yaml", "Secrets configuration file")
+	user := flag.String("user", "pojntfx", "Default username to omit from display (can also be set using the FORGE_USER env variable)")
 
 	flag.Parse()
 
@@ -69,18 +84,6 @@ func main() {
 		Level: level,
 	}))
 
-	if apiEnv := os.Getenv("FORGE_API"); apiEnv != "" {
-		*api = apiEnv
-	}
-
-	if cdnEnv := os.Getenv("FORGE_CDN"); cdnEnv != "" {
-		*cdn = cdnEnv
-	}
-
-	if tokenEnv := os.Getenv("FORGE_TOKEN"); tokenEnv != "" {
-		*token = tokenEnv
-	}
-
 	if userEnv := os.Getenv("FORGE_USER"); userEnv != "" {
 		*user = userEnv
 	}
@@ -88,33 +91,73 @@ func main() {
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
 
-	log.Info("Reading input file", "src", *src)
+	log.Info("Reading forges configuration", "file", *forgesFile)
 
-	input, err := os.ReadFile(*src)
+	forgesData, err := os.ReadFile(*forgesFile)
+	if err != nil {
+		panic(err)
+	}
+
+	var forgesList []ForgeConfig
+	if err := yaml.Unmarshal(forgesData, &forgesList); err != nil {
+		panic(err)
+	}
+
+	forges := map[string]ForgeConfig{}
+	for _, f := range forgesList {
+		forges[f.Domain] = f
+	}
+
+	log.Info("Reading secrets configuration", "file", *secretsFile)
+
+	secretsData, err := os.ReadFile(*secretsFile)
+	if err != nil {
+		panic(err)
+	}
+
+	var secretsList []ForgeSecret
+	if err := yaml.Unmarshal(secretsData, &secretsList); err != nil {
+		panic(err)
+	}
+
+	secrets := map[string]ForgeSecret{}
+	for _, s := range secretsList {
+		secrets[s.Domain] = s
+	}
+
+	clients := map[string]*github.Client{}
+	for domain, forge := range forges {
+		var httpClient *http.Client
+		if secret, ok := secrets[domain]; ok && secret.Token != "" {
+			httpClient = oauth2.NewClient(
+				ctx,
+				oauth2.StaticTokenSource(
+					&oauth2.Token{
+						AccessToken: secret.Token,
+					},
+				),
+			)
+		}
+
+		client := github.NewClient(httpClient)
+		client.BaseURL, err = url.Parse(forge.API)
+		if err != nil {
+			panic(err)
+		}
+
+		clients[domain] = client
+		log.Debug("Initialized client for forge", "domain", domain, "api", forge.API)
+	}
+
+	log.Info("Reading projects file", "file", *projectsFile)
+
+	input, err := os.ReadFile(*projectsFile)
 	if err != nil {
 		panic(err)
 	}
 
 	var parsedInput []InputCategory
 	if err := yaml.Unmarshal(input, &parsedInput); err != nil {
-		panic(err)
-	}
-
-	var httpClient *http.Client
-	if *token != "" {
-		httpClient = oauth2.NewClient(
-			ctx,
-			oauth2.StaticTokenSource(
-				&oauth2.Token{
-					AccessToken: *token,
-				},
-			),
-		)
-	}
-
-	client := github.NewClient(httpClient)
-	client.BaseURL, err = url.Parse(*api)
-	if err != nil {
 		panic(err)
 	}
 
@@ -128,22 +171,41 @@ func main() {
 		}
 
 		for _, inputProject := range inputCategory.Projects {
-			owner, repo := path.Split(inputProject.Slug)
-			ownerTrimmed := strings.TrimSuffix(owner, "/")
+			parts := strings.SplitN(inputProject.Repo, "/", 3)
+			if len(parts) != 3 {
+				panic(fmt.Errorf("invalid repo format, expected domain/owner/repo: %s", inputProject.Repo))
+			}
 
-			log.Debug("Fetching repository", "owner", ownerTrimmed, "repo", repo)
+			domain := parts[0]
+			owner := parts[1]
+			repo := parts[2]
 
-			project, _, err := client.Repositories.Get(ctx, ownerTrimmed, repo)
+			forge, ok := forges[domain]
+			if !ok {
+				panic(fmt.Errorf("unknown forge domain: %s", domain))
+			}
+
+			client, ok := clients[domain]
+			if !ok {
+				panic(fmt.Errorf("no client for forge domain: %s", domain))
+			}
+
+			log.Debug("Fetching repository", "domain", domain, "owner", owner, "repo", repo)
+
+			project, _, err := client.Repositories.Get(ctx, owner, repo)
 			if err != nil {
 				panic(err)
 			}
 
-			license := "UNLICENSED"
-			if l := project.GetLicense(); l != nil {
-				license = l.GetSPDXID()
+			license := ""
+			if forge.Type == "github" { // Codeberg doesn't expose the project license via the API
+				license = "UNLICENSED"
+				if l := project.GetLicense(); l != nil {
+					license = l.GetSPDXID()
+				}
 			}
 
-			commits, _, err := client.Repositories.ListCommits(ctx, ownerTrimmed, repo, &github.CommitsListOptions{})
+			commits, _, err := client.Repositories.ListCommits(ctx, owner, repo, &github.CommitsListOptions{})
 			if err != nil {
 				panic(err)
 			}
@@ -155,7 +217,12 @@ func main() {
 
 			icon := ""
 			if inputProject.Icon != "" {
-				icon = *cdn + owner + repo + "/" + project.GetDefaultBranch() + "/" + inputProject.Icon
+				// GitHub and Forgejo/Codeberg have different CDN URL formats
+				if forge.Type == "github" {
+					icon = forge.CDN + owner + "/" + repo + "/" + project.GetDefaultBranch() + "/" + inputProject.Icon
+				} else {
+					icon = forge.CDN + owner + "/" + repo + "/raw/branch/" + project.GetDefaultBranch() + "/" + inputProject.Icon
+				}
 			}
 
 			outputCategory.Projects = append(outputCategory.Projects, OutputProject{
@@ -170,6 +237,8 @@ func main() {
 				Forks:       project.GetForksCount(),
 				Issues:      project.GetOpenIssuesCount(),
 				Icon:        icon,
+				ForgeDomain: domain,
+				ForgeEmoji:  forge.Emoji,
 			})
 
 			log.Debug("Processed repository", "fullName", project.GetFullName(), "stars", project.GetStargazersCount())
@@ -202,18 +271,30 @@ func main() {
 					iconMarkdown = fmt.Sprintf("<img alt=\"Icon\" src=\"%s\" height=\"24\" align=\"top\"> ", html.EscapeString(project.Icon))
 				}
 
+				titleParts := strings.Split(project.Title, "/")
 				displayedTitle := project.Title
-				if strings.Split(project.Title, "/")[0] == *user {
-					displayedTitle = strings.Split(project.Title, "/")[1] // Use just the repo name if owner matches the GitHub user
+				if len(titleParts) == 2 {
+					if titleParts[0] == *user {
+						displayedTitle = titleParts[1]
+					} else {
+						displayedTitle = path.Join(titleParts[0], titleParts[1])
+					}
 				}
 
-				projectMarkdown := fmt.Sprintf("<a display=\"inline\" target=\"_blank\" href=\"%s\"><b>%s%s</b></a> (⭐ %d 🛠️ %s ⚖️ %s 📅 %s) <br>%s",
+				displayedTitle = project.ForgeEmoji + "/" + displayedTitle
+
+				licensePart := ""
+				if project.License != "" {
+					licensePart = fmt.Sprintf(" ⚖️ %s", html.EscapeString(project.License))
+				}
+
+				projectMarkdown := fmt.Sprintf("<a display=\"inline\" target=\"_blank\" href=\"%s\"><b>%s%s</b></a> (⭐ %d 🛠️ %s%s 📅 %s) <br>%s",
 					html.EscapeString(project.URL),
 					iconMarkdown,
 					html.EscapeString(displayedTitle),
 					project.Stars,
 					html.EscapeString(project.Language),
-					html.EscapeString(project.License),
+					licensePart,
 					formattedDate,
 					html.EscapeString(project.Description),
 				)
